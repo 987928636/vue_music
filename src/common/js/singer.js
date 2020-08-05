36fc605b65972f4d0c3c82a76f388a6bf225667e
yueyuye <987928636@qq.com>
2020-08-05 19:17:14 +0800

    project init

---
+++ src/common/js/singer.js
@@ -0,0 +1,15 @@
+export default class Singer {
+  constructor({id,name}){
+    this.id = id
+    this.name = name
+    this.avatar = `//y.gtimg.cn/music/photo_new/T001R150x150M000${id}.jpg?max_age=2592000`
+  }
+}
+
+// export default class Singer {
+//   constructor({id,name,avatar}){
+//     this.id = id
+//     this.name = name
+//     this.avatar = avatar
+//   }
+// }
---
+++ src/common/js/song.js
@@ -0,0 +1,22 @@
+//处理歌手数据
+export default class Song{
+  constructor({id,mid,singer,name,album,image}) {
+      this.id = id
+      this.mid = mid
+      this.singer =singer
+      this.name = name
+      this.album =album
+      this.image = image
+  }
+}
+
+export function settingSong(musicData){
+  return new Song({
+    id:musicData.songid,
+    mid:musicData.songmid,
+    singer:musicData.singer[0].name,
+    name:musicData.songname,
+    album:musicData.albumname,
+    image:`https://y.gtimg.cn/music/photo_new/T001R150x150M000${musicData.albummid}.jpg?max_age=2592000`
+  })
+}
